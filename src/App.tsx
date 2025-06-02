da4e2d3ab0e2773ea4bc22518a374796e42fd64a
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2025-06-02 08:33:12 -0400

    Add Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import { framer, CanvasNode } from "framer-plugin"
 import { useState, useEffect } from "react"
 import "./App.css"
 
-framer.showUI({
-    position: "top right",
-    width: 240,
-    height: 95,
-})
+export function App() {
+    const selection = useSelection()
+
+    useEffect(() => {
+        framer.showUI({
+            position: "top right",
+            width: 300,
+            height: 300,
+        })
+    }, [])
+
+    return (
+        <main className="flex-col p-3 w-full">
+            <button className="framer-button-primary">Insert Logo</button>
+        </main>
+    )
+}
 
 function useSelection() {
     const [selection, setSelection] = useState<CanvasNode[]>([])
@@ -16,31 +28,4 @@ function useSelection() {
     }, [])
 
     return selection
-}
-
-export function App() {
-    const selection = useSelection()
-    const layer = selection.length === 1 ? "layer" : "layers"
-
-    const handleAddSvg = async () => {
-        await framer.addSVG({
-            svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill="#999" d="M20 0v8h-8L4 0ZM4 8h8l8 8h-8v8l-8-8Z"/></svg>`,
-            name: "Logo.svg",
-        })
-    }
-
-    return (
-        <main>
-            <p>
-                Welcome! Check out the{" "}
-                <a href="https://framer.com/developers/plugins/introduction" target="_blank">
-                    Docs
-                </a>{" "}
-                to start. You have {selection.length} {layer} selected.
-            </p>
-            <button className="framer-button-primary" onClick={handleAddSvg}>
-                Insert Logo
-            </button>
-        </main>
-    )
 }
